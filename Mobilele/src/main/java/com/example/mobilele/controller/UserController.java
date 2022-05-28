f529ac9d5fdb6f2759951f4fee2d5d776804fdc7
Martin Bozhinov <sharkytrapera@gmail.com>
2022-05-28 20:55:05 +0300

    adding functionality

---
+++ Mobilele/src/main/java/com/example/mobilele/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.mobilele.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+
+    @GetMapping("/register")
+    public String register(){
+        return "auth-register";
+    }
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "auth-login";
+    }
+
+
+
+}
---
+++ Mobilele/src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.mobilele.service.impl;
+
+import com.example.mobilele.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+}
